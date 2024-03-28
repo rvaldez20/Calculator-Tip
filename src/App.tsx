cdb7428544e011eeca6357a782482fe42c93689f
rvaldez20 <rvaldez20@gmail.com>
2024-03-27 21:42:36 -0600

    [ADD] create Main with two colums: for Menu and Consumo

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,18 @@ function App() {
         <h1 className="text-center text-4xl font-black">Calculadora de Propinas y Consumo</h1>
       </header>
 
+      <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
+        <div>
+          <h2 className="">Menú</h2>
+
+        </div>
+
+        <div>
+          <h2 className="">Consumo</h2>
+        </div>
+      </main>
+
+
 
     </>
   )
