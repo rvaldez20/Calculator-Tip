394ebcdc64bbfffe521ff53764788dc42f22eb6b
rvaldez20 <rvaldez20@gmail.com>
2024-03-30 17:12:09 -0600

    [ADD] create component OrderTotal for show SubTotal, tip and Total

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import MenuItem from "./components/MenuItem"
 import OrderContents from "./components/OrderContents"
+import OrderTotals from "./components/OrderTotals"
 import { menuItems } from "./data/db"
 import useOrder from './hooks/useOrder'
 
@@ -35,6 +36,9 @@ function App() {
             order={order}
             removeItem={removeItem}
           />
+          
+          <OrderTotals />
+
         </div>
       </main>
 
