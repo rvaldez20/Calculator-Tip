5e9b671bd645d06bb643c9c266a1d07eeb374411
rvaldez20 <rvaldez20@gmail.com>
2024-03-29 23:17:50 -0600

    [ADD] create function removeItem for delete item that clic in button with X

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -19,11 +19,16 @@ export default function useOrder() {
          const newItem = {...item, quantity: 1}
          setOrder([...order, newItem])
       }
+   }
 
+
+   const removeItem = (id: MenuItem['id']) => {
+      setOrder(order.filter(item => item.id !== id))
    }
 
    return {
-      addItem,
       order,
+      addItem,
+      removeItem,
    }
 }
